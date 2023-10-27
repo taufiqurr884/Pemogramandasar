2aed1850edca5beedb85d699e55bdd4e1afbd30a
taufiqurr884 <149137911+taufiqurr884@users.noreply.github.com>
2023-10-27 19:36:02 +0700

    Create Latihan 1_variabel.py

---
+++ Latihan 1_variabel.py
@@ -0,0 +1,39 @@
+
+
+a = 4
+b = 8
+c = 2
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("penyelesaian:",(a+b)*(a-c))
+
+
+
+a = 10
+b = 5
+c = 4
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("penyelesaian:",(a*b)/(a+c))
+
+
+a = 3
+b = 7
+t = 6
+print("a:",a)
+print("b:",b)
+print("t:",t) 
+print("penyelesaian luas trapesium adalah:",1/2*(a+b)*t) 
+
+
+
+r = 7
+print("r:",r)
+print("keliling lingkaran adalah:",2*3.14*7)
+
+
+r = 7
+print("r:",r)
+print("Luas lingkaran adalah:",3.14*7*7)

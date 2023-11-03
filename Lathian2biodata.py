0b46d8b46514a2d61a7c973b7c231833ded87172
taufiqurr884 <149137911+taufiqurr884@users.noreply.github.com>
2023-11-03 19:53:57 +0700

    Create Lathian2biodata.py

---
+++ Lathian2biodata.py
@@ -0,0 +1,11 @@
+"biodata mahasiswa"
+nim=input("masukannim") 
+nama=input("masukannama") 
+kelas=input("masukankelas") 
+prodi=input("masukanprodi") 
+print("masukan biodata mahasiswa ") 
+Print("nim:" , nim)
+Print("nama:", nama) 
+Print("kelas:", kelas) 
+Print("prodi:", prodi) 
+
